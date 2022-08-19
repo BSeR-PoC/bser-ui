4407f7f23c94d44e6c0cd75b7b2d41da32b27ea0
Ellie Shivers <elliemshivers@gmail.com>
2022-08-19 19:35:26 -0400

    Working version of Provider Registration

--- src/app/service/service-provider-registration.service.ts
+++ src/app/service/service-provider-registration.service.ts
@@ -2,6 +2,19 @@ import { Injectable } from '@angular/core';
 import {map, Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {ServiceProviderService} from "./service-provider.service";
+import {Bundle, BundleEntry, BundleEntryRequest} from "@fhir-typescript/r4-core/dist/fhir/Bundle";
+import {Practitioner} from "@fhir-typescript/r4-core/dist/fhir/Practitioner";
+import {Organization} from "@fhir-typescript/r4-core/dist/fhir/Organization";
+import {PractitionerRole} from "@fhir-typescript/r4-core/dist/fhir/PractitionerRole";
+import {Endpoint} from "@fhir-typescript/r4-core/dist/fhir/Endpoint";
+import {Coding} from "@fhir-typescript/r4-core/dist/fhir/Coding";
+import {CodeableConcept} from "@fhir-typescript/r4-core/dist/fhir/CodeableConcept";
+import {Meta} from "@fhir-typescript/r4-core/dist/fhir/Meta";
+import {ContactPoint} from "@fhir-typescript/r4-core/dist/fhir/ContactPoint";
+import {HumanName} from "@fhir-typescript/r4-core/dist/fhir/HumanName";
+import {v4 as uuidv4} from 'uuid';
+import {Identifier, Reference, Resource} from "@fhir-typescript/r4-core/dist/fhir";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +32,135 @@ export class ServiceProviderRegistrationService {
         }
       )
     );
+  }
+
+  createNewServiceProvider(formData: any): Observable<any> {
+    let practitioner: Practitioner = undefined;
+    if (!(Object.values(formData.practitioner).every(value => value === null))) {
+      practitioner = this.buildBserPractitionerResource(formData.practitioner);
+    }
+    let endpoint = this.buildEndpointResource(formData.endpoint);
+    let organization = this.buildBserOrganizationResource(formData.organization);
+    let practitionerRole = this.buildBserRecipientPractitionerRole(practitioner, organization, endpoint);
+
+    let transBundle = new Bundle({type: "transaction"});
+
+    this.createBundlePostRequestEntries([practitionerRole, practitioner, organization, endpoint]).forEach(
+      bundleEntry => transBundle.entry.push(bundleEntry)
+    );
+    return this.http.post(environment.bserProviderServer, transBundle.toJSON())
+      //.pipe(
+      // map( result => {
+      //     console.log(result);
+      //     // TODO: Check Success. Is there a better way to do this??? This feels bad.
+      //     return this.getServiceProviders();
+      //   }
+      // )
+    //);
+  }
+
+  createBundlePostRequestEntries(resources: Resource[]): BundleEntry[] {
+    let bundleEntries = []
+    resources.forEach(resource => {
+      let bundleEntry = new BundleEntry({
+        fullUrl: resource.resourceType + "/" + resource.id,
+        resource: resource.toJSON(),
+        request: new BundleEntryRequest({
+          method: "POST",
+          url: resource.resourceType
+        })
+      });
+      bundleEntries.push(bundleEntry);
+    });
+    return bundleEntries;
+  }
+
+  buildBserRecipientPractitionerRole(practitioner, organization, endpoint): PractitionerRole {
+    let profile = "http://hl7.org/fhir/us/bser/StructureDefinition/BSeR-ReferralRecipientPractitionerRole";
+    let bserPractitionerRole = new PractitionerRole({
+      meta: new Meta({profile: [profile]}),
+      id: uuidv4(),
+      organization: Reference.fromResource(organization),
+      endpoint: [Reference.fromResource(endpoint)]
+    });
+    if (practitioner !== undefined) {
+      bserPractitionerRole.practitioner = Reference.fromResource(practitioner);
+    }
+    return bserPractitionerRole;
+  }
+
+  buildBserPractitionerResource(practitionerDetails: any): Practitioner {
+    let profile = "http://hl7.org/fhir/us/bser/StructureDefinition/BSeR-Practitioner";
+    // TODO: Rebuild this once validators in place. Practitioner should be all or nothing.
+    let practitioner = new Practitioner({
+      meta: new Meta({profile: [profile]}),
+      id: uuidv4()
+    });
+
+    if (practitionerDetails.npi !== null) {
+      let npiIdentifier = new Identifier({
+        system: "http://hl7.org.fhir/sid/us-npi",
+        value: practitionerDetails.npi
+      });
+      practitioner.identifier = [npiIdentifier];
+    }
+    if (practitionerDetails.phone !== null) {
+      let phoneContactPoint = new ContactPoint({
+        system: "phone",
+        use: "work",
+        value: practitionerDetails.phone
+      });
+      practitioner.telecom = [phoneContactPoint];
+    }
+
+    // TODO: May need to check for empty string.
+    if (practitionerDetails.givenName !== null || practitionerDetails.familyName !== null) {
+      let name = new HumanName()
+      if (practitionerDetails.givenName !== null) {
+        name.given = [practitionerDetails.givenName];
+      }
+      if (practitionerDetails.familyName !== null) {
+        name.family = practitionerDetails.familyName;
+      }
+      practitioner.name = [name];
+    }
+    return practitioner;
+  }
+
+  buildEndpointResource(endpoint: string): Endpoint {
+    return new Endpoint({
+      id: uuidv4().toString(),
+      status: "active",
+      connectionType: new Coding({system: "http://terminology.hl7.org/CodeSystem/endpoint-connection-type", code: "hl7-fhir-msg"}),
+      payloadType: [new CodeableConcept(
+        {text: "BSeR Referral Message"
+        })],
+      address: endpoint
+    })
+  }
+
+  buildBserOrganizationResource(organizationDetails: any): Organization {
+    let typeCoding = new Coding({
+      system: "http://terminology.hl7.org/CodeSystem/organization-type",
+      code: "prov",
+      display: "Healthcare Provider"
+    });
+
+    let phoneContactPoint = new ContactPoint({
+      system: "phone",
+      use: "work",
+      value: organizationDetails.phone
+    });
+
+    return new Organization({
+      meta: new Meta({
+        profile: ["http://hl7.org/fhir/us/bser/StructureDefinition/BSeR-Organization"]
+      }),
+      id: uuidv4(),
+      active: true,
+      type: [new CodeableConcept({coding: [typeCoding]})],
+      name: organizationDetails.name,
+      telecom: [phoneContactPoint]
+    });
   }
 }
--- src/app/service/service-provider.service.ts
+++ src/app/service/service-provider.service.ts
@@ -27,7 +27,7 @@ export class ServiceProviderService {
 
   public getServiceProviders() {
     let profile = "http://hl7.org/fhir/us/bser/StructureDefinition/BSeR-ReferralRecipientPractitionerRole";
-    let include = "&_include=PractitionerRole:practitioner&_include=PractitionerRole:organization" // TODO: ADD HEALTHCARE SERVICE INCLUDE
+    let include = "&_include=PractitionerRole:practitioner&_include=PractitionerRole:organization&_include=PractitionerRole:endpoint" // TODO: ADD HEALTHCARE SERVICE INCLUDE
     let connectionUrl = environment.bserProviderServer + "PractitionerRole?_profile=" + profile + include;
     return this.http.get(connectionUrl).pipe(
       map(results => {
@@ -42,8 +42,15 @@ export class ServiceProviderService {
     let practitionerRoles = results.entry.filter(entry => entry.resource.resourceType === "PractitionerRole");
     practitionerRoles.forEach(practitionerRole => {
         // IF NEITHER PRACTITIONER NOR ORGANIZATION DISCARD AS INVALID
-        if ("practitioner" in practitionerRole.resource || "organization" in practitionerRole.resource){
-          let serviceProvider = { "practitionerRole": practitionerRole.resource, "practitioner": undefined, "organization": undefined, "location": undefined};
+        if ("organization" in practitionerRole.resource){
+          let serviceProvider = {
+            "practitionerRole": practitionerRole.resource,
+            "practitioner": undefined,
+            "organization": undefined,
+            "location": undefined,
+            "endpoint": undefined,
+            "healthcareService": undefined
+          };
 
           if ("practitioner" in practitionerRole.resource) {
             serviceProvider.practitioner = (results.entry.find(entry => entry.fullUrl.endsWith(practitionerRole.resource.practitioner.reference))).resource;
@@ -52,32 +59,49 @@ export class ServiceProviderService {
             serviceProvider.organization = (results.entry.find(entry => entry.fullUrl.endsWith(practitionerRole.resource.organization.reference))).resource;
           }
           if ("location" in practitionerRole.resource) {
-            // TODO: Implement Location
+            serviceProvider.location = (results.entry.find(entry => entry.fullUrl.endsWith(practitionerRole.resource.location[0].reference))).resource;
+          }
+          if ("endpoint" in practitionerRole.resource) {
+            serviceProvider.endpoint = (results.entry.find(entry => entry.fullUrl.endsWith(practitionerRole.resource.endpoint[0].reference))).resource;
+          }
+          if ("healthcareService" in practitionerRole.resource) {
+            serviceProvider.healthcareService = (results.entry.find(entry => entry.fullUrl.endsWith(practitionerRole.resource.healthcareService[0].reference))).resource;
           }
           let serviceProviderObj = this.createServiceProviderObj(serviceProvider)
           serviceProviderList.push(serviceProviderObj)
         }
         else {
-          console.log("Invalid PractitionerRole with id " + practitionerRole.resource.id + ". Requires either or both of Practitioner and Organization.")
+          console.log("Invalid PractitionerRole with id " + practitionerRole.resource.id + ". Requires Organization.")
         }
 
       }
     )
-    return serviceProviderList
+    return serviceProviderList;
   }
 
   private createServiceProviderObj(serviceProvider: any) {
+    //TODO: Add more complex name and telecom handling.
     return {
       "serviceProviderId": serviceProvider.practitionerRole.id,
-      "practitionerName" : this.getFullName(serviceProvider.practitioner.name),
-      "organizationName" : serviceProvider.organization?.name,
-      "description": "Placeholder description",
-      "selected": false
+      "practitioner": {
+        "givenName": serviceProvider.practitioner?.name[0]?.given[0] || null,
+        "familyName": serviceProvider.practitioner?.name[0]?.family || null,
+        "phone": serviceProvider.practitioner?.telecom[0]?.value || null,
+        "npi": serviceProvider.practitioner?.identifier[0]?.value || null
+      },
+      "organization": {
+        "name": serviceProvider.organization.name,
+        "phone": serviceProvider.organization.telecom[0]?.value || null
+      },
+      "endpoint": serviceProvider.endpoint.address,
+      "selected": false,
+      "resources": serviceProvider
     }
   }
 
+  // TODO: Add "use" handling.
   private getFullName(humanName: HumanName[]){
-    console.log(humanName)
+
     return "Test Name"
   }
 }
