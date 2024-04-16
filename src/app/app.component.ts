b9b56a198cf1268ac7aeb2df70e5f67042e488c4
Plamen Tassev <plamen@gatech.edu>
2024-04-16 08:47:15 -0400

    reverted code change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {FhirClientService} from "./service/fhir-client.service";
-import {ServiceRequestHandlerService} from "./service/service-request-handler.service";
-import {Router} from "@angular/router";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  constructor(private serviceRequestHandlerService: ServiceRequestHandlerService, private router: Router, private fhirClient: FhirClientService) {
-
-  }
-
-  ngOnInit(): void {
-    this.serviceRequestHandlerService.isClientInitialized$.subscribe(value=> {
-      console.log(value);
-      if(!value){
-
-      }
-      else{
-        this.router.navigate(['/'])
-      }
-    })
-  };
-
+export class AppComponent {
+  title = 'bser-ui';
 }
